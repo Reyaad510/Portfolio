f59cc51073a17c8191f17dfdffb8d8dc13e2d9a3
Reyaad Rafeek <reyaad510@gmail.com>
2020-01-03 18:38:02 -0500

    Messing around with react-spring

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,38 @@
 import React from "react";
+import { Trail } from "react-spring/renderprops";
+import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1> Hello world!</h1>
-    </div>
-  );
+const items = ["H", "e", "l", "l", "o", "!"];
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Parallax pages={3} ref={ref => (this.parallax = ref)}>
+          <ParallaxLayer
+            offset={1}
+            speed={1}
+            style={{ backgroundColor: "#805E73" }}
+          />
+          <ParallaxLayer
+            offset={2}
+            speed={1}
+            style={{ backgroundColor: "#87BCDE" }}
+          />
+        </Parallax>
+
+        {/* <Trail
+          items={items}
+          keys={item => item.key}
+          from={{ transform: "translate3d(0,-1000px,0)" }}
+          to={{ transform: "translate3d(0,0px,0)" }}
+        >
+          {item => props => <span style={props}>{item}</span>}
+        </Trail> */}
+      </div>
+    );
+  }
 }
 
 export default App;
