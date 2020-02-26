880d67c4d7ec7bfc2b1a167da1cfa2baef1edda8
Reyaad Rafeek <reyaad510@gmail.com>
2020-02-25 22:37:55 -0500

    Added media queries for mobile

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -12,7 +12,6 @@ import { Spring, Trail, animated } from "react-spring/renderprops";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelopeOpen } from "@fortawesome/free-solid-svg-icons";
-// import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons";
 
 library.add(fab, faEnvelopeOpen);
 
@@ -65,10 +64,13 @@ class App extends React.Component {
             <video loop autoPlay muted>
               <source src={sjVideo} />{" "}
             </video>
-            <p>
-              S&J Architect is a static website made to show that I am able to
-              create a website after being given a design from product manager.
-            </p>
+            <div className="p-contain">
+              <p>
+                S&J Architect is a static website made to show that I am able to
+                create a website after being given a design from product
+                manager.
+              </p>
+            </div>
 
             <button>
               {" "}
@@ -86,12 +88,14 @@ class App extends React.Component {
             <video loop autoPlay muted>
               <source src={bhVideo} />{" "}
             </video>
-            <p>
-              Sometimes, you just need to vent to get it out, move on, and feel
-              better. Enter black hole. Blackhole is a reverse note taking app.
-              Type away your frustrations and watch it get sucked into the
-              blackhole!
-            </p>
+            <div className="p-contain">
+              <p className="p-contain">
+                Sometimes, you just need to vent to get it out, move on, and
+                feel better. Enter black hole. Blackhole is a reverse note
+                taking app. Type away your frustrations and watch it get sucked
+                into the blackhole!
+              </p>
+            </div>
             <button>
               {" "}
               <a
@@ -108,10 +112,12 @@ class App extends React.Component {
             <video loop autoPlay muted>
               <source src={bucketVideo} />{" "}
             </video>
-            <p>
-              Welcome to the BucketList. A simple app that allows a user to
-              create bucket list cards.
-            </p>
+            <div className="p-contain">
+              <p>
+                Welcome to the BucketList. A simple app that allows a user to
+                create bucket list cards.
+              </p>
+            </div>
             <button>
               {" "}
               <a
@@ -128,11 +134,13 @@ class App extends React.Component {
             <video loop autoPlay muted>
               <source src={peVideo} />{" "}
             </video>
-            <p>
-              The Photo Effects App was created so you could build beautiful and
-              meaningful messages over photos with ease without the complexity
-              of Photoshop and Canva.
-            </p>
+            <div className="p-contain">
+              <p>
+                The Photo Effects App was created so you could build beautiful
+                and meaningful messages over photos with ease without the
+                complexity of Photoshop and Canva.
+              </p>
+            </div>
             <button>
               {" "}
               <a
@@ -156,29 +164,3 @@ export default App;
 /*
 colors: sandy: #faefe8, green: #8ce5b0, green2: #a4eebb, blue: #7cdbe1, blue2:#8bdbeb
 */
-
-// const items = ["H", "e", "l", "l", "o", "!"];
-
-/* <Parallax pages={3} ref={ref => (this.parallax = ref)}>
-          <ParallaxLayer
-            offset={1}
-            speed={1}
-            style={{ backgroundColor: "#805E73" }}
-          />
-          <ParallaxLayer
-            offset={2}
-            speed={1}
-            style={{ backgroundColor: "#87BCDE" }}
-          />
-
-          <ParallaxLayer offset={0} speed={0} factor={3} className="back" />
-        </Parallax> */
-
-/* <Trail
-          items={items}
-          keys={item => item.key}
-          from={{ transform: "translate3d(0,-1000px,0)" }}
-          to={{ transform: "translate3d(0,0px,0)" }}
-        >
-          {item => props => <span style={props}>{item}</span>}
-        </Trail> */
